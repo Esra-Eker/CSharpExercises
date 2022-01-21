2420c6c3885c57a0d879b1ae516776bd9245448e
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:09:49 +0300

    While örnek

---
+++ WhileDongusu/WhileDongusu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = 1, top=0;
+
+            while(sayi != 0)
+            {
+                Console.WriteLine("Sayı Gir : ");
+                sayi = int.Parse(Console.ReadLine());
+
+                top += sayi;
+            }
+
+
+            Console.WriteLine("Girdiğiniz Sayıların Toplamları = {0}", top);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
