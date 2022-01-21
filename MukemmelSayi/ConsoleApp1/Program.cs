d20e278b6590d3d785a0cca975420960542211fb
esra-eker <elifesraeker@gmail.com>
2022-01-21 20:58:43 +0300

    Mükemmel sayı

---
+++ MukemmelSayi/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bi sayı gir: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int bolen, toplam=0;
+
+            for (int i=1; i<sayi; i++)
+            {
+               if(sayi%i==0)
+                {
+                    toplam += i;
+                }
+            }
+            if (toplam == sayi)
+            {
+                Console.WriteLine("Mükemmek sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine("Mükemmel sayı değildir.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
