ba928df0e1609811743fa2e5064a7b57dd17b943
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:10:25 +0300

    While örnek

---
+++ WhileOrnek/ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, toplam=0;
+
+            Console.Write("Bi sayı gir: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            while (sayi%i ==0)
+            {
+                toplam += i;
+                i++;
+            }
+            Console.WriteLine(toplam + sayi);
+            Console.ReadLine();
+        }
+
+    }
+}
