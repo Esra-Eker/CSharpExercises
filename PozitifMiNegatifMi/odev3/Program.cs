378ff7dc90f2467339f40dfb7f64a44f04a162ab
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:45:50 +0300

    Pozitif negatif kontrol

---
+++ PozitifMiNegatifMi/odev3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Bi sayı giriniz: ");
+            sayi = int.Parse(Console.ReadLine());
+
+            if (sayi>0)
+            {
+                Console.WriteLine("Girilen sayı pozitiftir.");
+            }
+            else if (sayi<0)
+            {
+                Console.WriteLine("Girilen sayı negatiftir.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı 0'dır");
+            }
+            Console.ReadLine();
+        }
+    }
+}
