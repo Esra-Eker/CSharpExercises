f59870d8f8c49ee5c85ee2da8fd3a7700d59a913
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:08:22 +0300

    Do while 3

---
+++ DoWhile3/dowhileornek/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhileornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int i = 1,sayi, toplam=0;
+              do
+              {
+                  Console.Write(i+ ". sayıyı gir: ");
+                  sayi = int.Parse(Console.ReadLine());
+                  toplam += sayi;
+                  i++;
+              } while (i<11);
+            Console.WriteLine("Toplam: " + toplam);*/
+
+
+           /* Random X = new Random();
+            int RastgeleSayi = X.Next(0,101);
+            int sayi, sayac=0;
+
+            Console.WriteLine(RastgeleSayi);
+
+
+            do
+            {
+                Console.Write("Sayı gir: ");
+                sayi = int.Parse(Console.ReadLine());
+                sayac++;
+            } while (RastgeleSayi != sayi);
+
+            if (RastgeleSayi== sayi)
+            {
+                Console.WriteLine("Tebrikler!");
+                Console.WriteLine("Sayıyı " + sayac + ". adımda bildiniz.");
+            }*/
+
+
+          
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
