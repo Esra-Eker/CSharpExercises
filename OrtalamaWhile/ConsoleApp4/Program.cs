25961aae447322b9773c9bf73981ad401500fb40
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:00:01 +0300

    While ile ortalama bulma

---
+++ OrtalamaWhile/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1 , sayi, toplam=0;
+            while (i<=5)
+            {
+                Console.Write(i + ". sayıyı gir: ");
+                sayi = int.Parse(Console.ReadLine());
+                toplam += sayi;
+                i++;
+            }
+            Console.WriteLine("Ortalaması: " + (double) toplam / 5);
+            Console.ReadKey();
+        }
+    }
+}
