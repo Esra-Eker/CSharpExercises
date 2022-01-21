3f620a4a4b8fc8490eb5a24df71ff58d4964bb22
esra-eker <elifesraeker@gmail.com>
2022-01-21 20:54:34 +0300

    Do while

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sayi, toplam=0;
+            do
+            {
+                Console.Write("Sayı Gir : ");
+                Sayi = int.Parse(Console.ReadLine());
+                toplam += Sayi;
+            } while (Sayi!=0);
+
+            Console.WriteLine("Toplamlar = {0}", toplam);
+            Console.ReadLine();
+        }
+    }
+}
