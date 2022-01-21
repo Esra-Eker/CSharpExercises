719984ae94f7361c7f9158c4937cd5760be456d7
esra-eker <elifesraeker@gmail.com>
2022-01-21 20:55:44 +0300

    Fibonacci

---
+++ Fibonacci/fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int onceki = 1, sonraki=1;
+            Console.WriteLine(onceki);
+
+            for (; ; )
+            {
+                Console.WriteLine(sonraki);
+                sonraki = onceki + sonraki;
+                onceki = sonraki - onceki;
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
