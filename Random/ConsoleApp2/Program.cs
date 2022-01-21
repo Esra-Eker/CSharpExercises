a8aed249f58590608518a646d86c7f65ea9829f7
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:05:55 +0300

    Random

---
+++ Random/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random X = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+
+                int UretilenRastgeleSayi = X.Next(9,20); // 10 dahil 20 dahil değil.
+                Console.WriteLine(UretilenRastgeleSayi);
+            }
+                               
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
