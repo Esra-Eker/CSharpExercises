d54da79b777a7cb646f3f23ac7886ac1cad5e100
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:53:28 +0300

    İlk 100 sayının bölenleri

---
+++ Ilk100SayininBolenleri/odev11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<=100; i++)
+            {
+                Console.Write(i + " sayısının bölenleri: ");
+                for (int j=1; j<=i; j++)
+                {
+                    if (i%j == 0)
+                    {
+                        Console.Write(i / j + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
