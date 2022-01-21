5db4736439776c152f6503787097adccf31dff3e
esra-eker <elifesraeker@gmail.com>
2022-01-21 20:57:03 +0300

    If Statement

---
+++ IfStatement/BarislaDerslerIfStatement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarislaDerslerIfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* int a, b, c, d;
+            Console.WriteLine("4 tane sayı gir: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());*/
+
+            /*if (a>=b && b>=c && c>=d)
+            {
+                Console.WriteLine("En büyük : " + a);
+            }
+
+           else if(b>=a && a>=c && c>=d)
+            {
+                Console.WriteLine("En büyük : " + b);
+            }
+            
+    
+           else if(c>=a && a>=b && b>=d)
+            {
+                Console.WriteLine("En büyük : " + c);
+            }
+
+            else if (d>=a && a>=b && b>=c)
+            {
+                Console.WriteLine("En büyük : " + d);
+            }
+          */
+
+            int sayi1 = 5, sayi2 =6;
+            string adsoyad;
+
+            adsoyad = "esra eker";
+
+            Console.WriteLine("Birinci sayı {0}, ikinci sayı {1}", sayi1, sayi2);
+            Console.WriteLine("Benim adım {0}", adsoyad);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
