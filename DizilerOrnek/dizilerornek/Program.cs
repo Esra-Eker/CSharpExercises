07903086616149b5d805b1aaf9ea1135f9bc8c43
esra-eker <elifesraeker@gmail.com>
2022-01-21 20:50:10 +0300

    Diziler örnek

---
+++ DizilerOrnek/dizilerornek/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizilerornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+
+            for (int  i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write("Sayı Gir : ");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            foreach(int sayi in sayilar)
+            {
+                if(sayi%2==0)
+                {
+                    Console.WriteLine(sayi);
+                }
+            }
+            Console.ReadLine();
+
+           
+
+        }
+    }
+}
