235ee8122bacbf64bc0f9da0e0e702e16cfa20f6
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:51:33 +0300

    Asal kontrol

---
+++ AsalKontrol/odev9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bi sayı gir: ");
+            int sayi = int.Parse(Console.ReadLine());
+            bool asalkontrol = false;
+
+            for (int i=2; i<sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    asalkontrol = false;
+                    Console.WriteLine(sayi + " asal sayı değildir.");
+                    break;
+                }
+                else 
+                {
+                    asalkontrol = true;
+                }
+            }
+            if (asalkontrol == true)
+            {
+                Console.WriteLine(sayi + " asal sayıdır.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
