de4e183fc667ae3a373ec54efccfdbae643689ac
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:50:51 +0300

    Bölenler

---
+++ Bolenler/odev8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bi sayı gir: ");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.Write("Bölenleri: ");
+            for (int i=1; i<=sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    Console.Write((sayi / i) + " ");
+                }
+                             
+            }
+            Console.ReadLine();
+        }
+    }
+}
