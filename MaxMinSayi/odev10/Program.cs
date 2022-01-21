d96b97862fc69d9ffc38f296721717bbb6f8b61b
esra-eker <elifesraeker@gmail.com>
2022-01-21 21:52:19 +0300

    Max min sayı

---
+++ MaxMinSayi/odev10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            int max = 0;
+            Console.Write("Sayı gir: ");
+            sayi = int.Parse(Console.ReadLine());
+            int min = sayi;
+            for (; ; )
+            {
+                Console.Write("Sayı gir: ");
+                sayi = int.Parse(Console.ReadLine());
+
+                if (sayi !=0)
+                {
+                    if (sayi > max)
+                    {
+                        max = sayi;
+                    }
+                    if (min > sayi)
+                    {
+                        min = sayi;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(max + " en büyük sayıdır.");
+            Console.WriteLine(min + " en küçük sayıdır.");
+            Console.ReadKey();
+        }
+    }
+}
