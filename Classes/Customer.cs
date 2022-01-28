3af349c37f2ab0d4cbb886354013b36f45c60c74
esra-eker <elifesraeker@gmail.com>
2022-01-28 19:14:02 +0300

    Sınıflarla Çalışmak

---
+++ Classes/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Customer
+    {
+        public int Id { get; set; } //Bir müşterinin özelliklerini tutmak için kullandığımız nesne.
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();  //class ı kullanabilmemiz için onun newlememiz gerek. Yani bir örneğini oluşturmalıyız.
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Esra";
+            customer.LastName = "Eker";
+
+            //Aşağıda ve yukarda yaptığımız tanımlamalar aynı. 2 türlü de yapılabilir.
+            Customer customer2 = new Customer
+            {
+                Id = 2, City ="İstanbul", FirstName ="Elif", LastName="Eker"
+            };
+
+            Console.WriteLine(customer2.FirstName); //customer2 nin FirstName ini ekrana yazdır demek.
+
+            Console.ReadLine();
+          
+        }
+    }
+
+    //class oluşturulurken kelimelerin ilk harfi büyük olur (PascalCase), örneği oluştururken kelimelerin sadece ilk harfi küçük (camelCase) yazılır.
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated!");
+        }
+    }
+
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Product Updated!");
+        }
+    }
+}
