fb44595d429e83019a763482f5c8708d535dcc2f
esra-eker <elifesraeker@gmail.com>
2022-01-29 23:11:11 +0300

    Interface ile çalışmak

---
+++ Interfaces/Interfaces1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             PersonManager manager = new PersonManager();
+            /* manager.Add(new Customer { Id = 1, FirstName = "Esra", LastName = "Eker", Address = "İstanbul" });*/
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Esra",
+                LastName = "Eker",
+                Address = "İstanbul"
+            };
+            manager.Add(customer);
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Elif",
+                LastName = "Eker",
+                Departmant = "Maths"
+            };
+            manager.Add(student);
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+
+    class Student: IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)  
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
---
+++ Interfaces/Interfaces2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  CustomerManager customerManager = new CustomerManager();
+              customerManager.Add(new SqlServerCustomerDal()); */
+
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+            Console.ReadLine();
+        }
+    }
+}
