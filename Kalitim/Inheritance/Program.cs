1730e66f5e74d55d5761b49555ba9654ec5e54bf
esra-eker <elifesraeker@gmail.com>
2022-01-30 22:23:55 +0300

    Kalıtım

---
+++ Kalitim/Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName="Esra" }, new Student{FirstName="Elif" }, new Person{FirstName="Deniz"}
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person   //class ların tek başına bir anlamı vardır ama interface lerin tek başına bir anlamı yoktur.
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person   //Person ı Customer ın babası gibi düşünebiliriz. Bir kişinin bir babası olacağından Person2 diye yeni bi class açsak bile yanına yazamayız. Yani bir neseneye sadece 1 kere inheritance alabiliriz ama birden fazla implementasyon yapabiliriz.
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person  //Hem inheritance hem de interface yazılacağı zaman inheritance önce yazılır.
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ Kalitim/VirtualMethods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySql mySql = new MySql();
+            mySql.Add();
+
+            Console.ReadLine();
+
+        }
+    }
+    //inheritance yaptığımız ortamda genel operasyonu (değişkenlik gösteriyorsa) ezmek istediğimiz zaman virtual u kullanabiliriz.
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+    class SqlServer:Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by Sql Code");  //Sqlde virtual u overried ettiğimiz için bu kod çalıştı.
+            //base.Add();  //base deki Add i yaz demek. temel kodu çalıştırır.
+        }
+    }
+    class MySql:Database  // MySqlde virtual u override etmediğimiz için temel kod çalıştı.
+    {
+
+    }
+}
+//bunu interface ile yapamayız. Böyle bi durum varsa inheritance ile yapmalıyız.
