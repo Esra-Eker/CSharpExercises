71edfce2d6bf97c5215e8c13cffd3e5252facf22
esra-eker <elifesraeker@gmail.com>
2022-01-31 20:14:57 +0300

    örnek

---
+++ Methods/methodOrnek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodOrnek
+{
+    class Program
+    {
+        public static int Cıkar(int sayi1,int sayi2)
+        {
+            int c = sayi1 - sayi2;
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İşlemin sonucu: " + Cıkar(7, 5));
+            Console.ReadLine();
+        }
+    }
+}
