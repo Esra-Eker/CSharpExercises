28f2467b7ae8fee7b6e52d39dca203d63fe055b2
esra-eker <elifesraeker@gmail.com>
2022-01-31 22:10:28 +0300

    get set

---
+++ get-set-kavrami/Ogrenci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_set_kavrami
+{
+    class Ogrenci
+    {
+        private string ad;
+        private string soyad;
+        private int yas;
+        private string alan;
+
+        public string ADI
+        {
+            get { return ad; }  //klavyeden girilecek olan ad değerini döndür (karşıma çıkar) demek.
+            set { ad = value; } //klavyeden girilen value değeri benim ad ım olacak.
+        }
+        public string SOYADI
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+        public string ALANI
+        {
+            get { return alan; }
+            set { alan = value; }
+        }
+        public int YASI
+        {
+            get { return yas; }
+            set                  
+            {
+                if (value < 18)
+                {
+                    yas = 18;
+                }
+                else
+                {
+                    yas = value;   //yaşı 18den büyük olanlarda değeri yas a aktar ama 18den küçük olanlarda yas a hep 18 değerini yaz. 
+                }
+            }
+
+        }
+    }
+}
---
+++ get-set-kavrami/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_set_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci(); //Ogrenci class ını ogr adındaki bir nesneyle çağırdık.
+            ogr.ADI = "Esra";
+            ogr.SOYADI = "Eker";
+            ogr.ALANI = "Sayısal";
+            ogr.YASI = 5;
+
+            Console.WriteLine("Adı: " + ogr.ADI);
+            Console.WriteLine("Soyadı: " + ogr.SOYADI);
+            Console.WriteLine("Alanı: " + ogr.ALANI);
+            Console.WriteLine("Yaşı: " + ogr.YASI);
+
+            Console.ReadLine();
+        }
+    }
+}
