83096bf00daa3863324fc28439d0b482b9671757
esra-eker <elifesraeker@gmail.com>
2022-02-01 22:23:48 +0300

    Çok biçimlilik örneği (virtual,override)

---
+++ cok_bicimlilik_ornek/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cok_bicimlilik_ornek
+{
+    class insan
+    {
+        public virtual void selamver()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Title = "Örnek Şubat 2022";
+            Console.WriteLine("Merhaba soğuk kış günleri");
+        }
+    }
+
+    class Türk:insan
+    {
+        public override void selamver()  //override selamver i geçersiz kılıyor.
+        {
+            //base.selamver();  temel selamver metodunun çıktısını yazar.
+            Console.WriteLine("Merhabalar");
+        }
+    }
+
+
+    class Fransız :insan
+    {
+        public override void selamver()
+        {
+            Console.WriteLine("Bonjour");
+        }
+    }
+
+    class İngiliz:insan
+    {
+        public override void selamver()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Türk trk = new Türk();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Write("Türklerin selam verme şekli: ");
+            trk.selamver();
+
+            Fransız frz = new Fransız();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("Fransızların selam verme şekli: ");
+            frz.selamver();
+
+            İngiliz ing = new İngiliz();
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Write("İngilizlerin selam verme şekli: ");
+            ing.selamver();
+            
+            Console.Read();
+        }
+    }
+}
---
+++ cok_bicimlilik_ornek/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("cok_bicimlilik_ornek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cok_bicimlilik_ornek")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64f5cf06-9f10-44c7-bfb8-a1202ab4210c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
