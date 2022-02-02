78a4b6f0c9e80c86dcd27066e9c56ab567122ce5
esra-eker <elifesraeker@gmail.com>
2022-02-02 22:35:38 +0300

    ref out

---
+++ ref-out-kavrami/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_out_kavrami
+{
+    class Program
+    {
+        //Eğer değişkenimi main metodunun içinde tanımlayarak yukarıdaki metodun referansını kullanmak istiyorsam ref kullanıyorum.
+        //Ama method içerisinde tanımlayacaksam out kullanıyorum.
+
+
+       /* static void arttir(ref int s)
+        {
+            s++;
+        }
+        static void Main(string[] args)
+        {
+            int a = 9;
+            arttir(ref a);
+            Console.Write(a);
+            Console.Read();
+
+        }  */
+
+        static void arttir(out int s)
+        {
+            s = 12;
+            s++;
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            arttir(out a);
+            Console.Write(a);
+            Console.Read();
+        }
+    }
+
+
+        
+}
+
