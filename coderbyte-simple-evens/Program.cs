0ee74e8f7e995bfd896f5fb71bbd8b0f8320efd1
esra-eker <elifesraeker@gmail.com>
2022-03-07 22:45:33 +0300

    coderbyte simple evens çözümü

---
+++ coderbyte-simple-evens/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornekcalisma
+{
+    class Program
+    {
+        //Coderbyte: Simple Evens
+        public static string SimpleEvens(long num)
+        {
+
+            if (num % 2 == 0)
+                return "true";
+            else
+                return "false";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SimpleEvens(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
