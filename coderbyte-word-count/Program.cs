4bd889f6d41989069c94b8febbe4cf786a6460a3
esra-eker <elifesraeker@gmail.com>
2022-03-07 22:46:12 +0300

    coderbyte word count çözümü

---
+++ coderbyte-word-count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coderbyte_ornek_calisma
+{
+    //Coderbyte: Word Count 
+    class Program
+    {
+        public static int WordCount(string str)
+        {
+            string[] kelimeler = str.Split(' ');
+            int sayac = 0;
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                sayac++;
+            }
+            return sayac;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordCount(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
